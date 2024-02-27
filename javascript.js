f6542dc4c7ba982aa7e6453115e845f61513a6cb
dizmerly <dan.izmerly@gmail.com>
2024-02-27 12:58:24 -0600

    Added Random Color functionality and button

--- javascript.js
+++ javascript.js
@@ -10,9 +10,25 @@ sketchContainer.setAttribute("style", "display:flex; justify-content: center; ma
 //Create window to draw grids inside of 
 const sketchWindow = document.createElement("div");
 sketchWindow.classList.add("sketch");
-sketchWindow.setAttribute("style", "background-color: lightblue; width: 400px; height: 400px; display: flex; flex-wrap: wrap; padding:0px; margin:0px; border: 1px solid lightblue");
+sketchWindow.setAttribute("style", "background-color: lightblue; width: 400px;\
+ height: 400px; display: flex; flex-wrap: wrap; padding:0px; margin:0px; border: 1px solid lightblue");
 sketchContainer.appendChild(sketchWindow);
 
+
+//Random Color button
+// const randomColor = Math.floor(Math.random()*16777215).toString(16);
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+let randomColorSwitch = 0; 
+const randomGridBtn = document.createElement("button");
+randomGridBtn.textContent = "Random Colors"
+randomGridBtn.setAttribute("style", "background-color: pink; padding: 20px;width 200px; height 100px; border-radius:10px;");
 
 
 
@@ -34,10 +50,16 @@ let setGrid = (gridNum) => {
         grid.setAttribute("id", "grid");
         let gridWidth = 100 / gridNum;  
         grid.addEventListener('mousemove', (e) => {
+            if(randomColorSwitch == 1){
+                grid.style.backgroundColor = `${getRandomColor()}`;
+            }
+            else{                         
             grid.style.backgroundColor = "black";
+            }
                 
         });
-        grid.setAttribute("style" , `background-color: white; width: ${gridWidth}%; height: ${gridWidth}%;
+        grid.setAttribute("style" , `background-color: white; \
+        width: ${gridWidth}%; height: ${gridWidth}%;
         box-shadow: inset 0 0 1px black;; margin: 0px; padding: 0px;`);
         
         sketchWindow.appendChild(grid);
@@ -57,12 +79,16 @@ const resetGridBtn = document.createElement("button");
 const btnContainer = document.createElement("div");
 setGridBtn.textContent = "Set Grid";
 resetGridBtn.textContent = "Reset Grid";
-setGridBtn.setAttribute("style", "background-color: pink; padding: 20px; width 200px; height 100px; border-radius:10px;")
-resetGridBtn.setAttribute("style", "background-color: pink; padding: 20px; width 200px; height 100px; border-radius:10px;")
-btnContainer.setAttribute("style", "display:flex; justify-content: center; margin-top: 40px; gap: 40px;")
+setGridBtn.setAttribute("style", "background-color: pink; padding: 20px; \
+ width 200px; height 100px; border-radius:10px;")
+resetGridBtn.setAttribute("style", "background-color: pink; padding: 20px; \
+ width 200px; height 100px; border-radius:10px;")
+btnContainer.setAttribute("style", "display:flex; justify-content: center;\
+ margin-top: 40px; gap: 40px;")
 body.insertBefore(btnContainer, sketchContainer);   
 btnContainer.appendChild(setGridBtn);
 btnContainer.appendChild(resetGridBtn);
+btnContainer.appendChild(randomGridBtn);
 
 
 setGridBtn.addEventListener('click', (event) => {
@@ -77,12 +103,32 @@ setGridBtn.addEventListener('click', (event) => {
     
 });
 
+//Reset the sketch window back to white 
 resetGridBtn.addEventListener('click', (e) => {
     let grids = document.querySelectorAll("#grid");
     grids.forEach((grid) => grid.style.backgroundColor = "white");
 
 });
 
+
+//Enable and disable random colors
+randomGridBtn.addEventListener('click', (e) => {
+    switch(randomColorSwitch)
+    {
+        case 0:
+            randomColorSwitch = 1;
+            randomGridBtn.style.backgroundColor = "#a83b3b";
+            console.log("turned on")
+            break;
+        case 1:
+            randomColorSwitch = 0;
+            randomGridBtn.style.backgroundColor = "pink"
+            console.log("turned off")
+            break;
+    }
+    
+
+});
 
 
 
